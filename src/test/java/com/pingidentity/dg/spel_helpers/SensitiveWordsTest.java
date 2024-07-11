6db84fdfc2b09a86a7ec00653831363b4e492cc3
paulbradyping <paulbrady@pingidentity.com>
2024-07-11 10:09:45 -0400

    Update SensitiveWordsTest.java

--- src/test/java/com/pingidentity/dg/spel_helpers/SensitiveWordsTest.java
+++ src/test/java/com/pingidentity/dg/spel_helpers/SensitiveWordsTest.java
@@ -1,1 +1,31 @@
+package com.pingidentity.dg.spel_helpers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests {@link SensitiveWords}.
+ *
+ */
+public class SensitiveWordsTest {
+
+  @Test
+  public void shouldRedactContent() {
+    Assert.assertEquals("The k***** is you!",
+        SensitiveWords.redactContent("The killer is you!"));
+  }
+
+  @Test
+  public void shouldUseExistingCase() {
+    Assert.assertEquals("The K***** is you!",
+        SensitiveWords.redactContent("The KiLlEr is you!"));
+  }
+
+  @Test
+  public void shouldIgnoreDefaultsWhenUsingExternalFile() {
+    Assert.assertEquals("Programmers are the worst.",
+        SensitiveWords.redactContent("Programmers are the worst."));
+  }
+
+}
 
